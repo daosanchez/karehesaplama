60f52c5ede1469e6b2bbcf5c314e6cf2b69faf19
ravza <145916188+ravzasanchez@users.noreply.github.com>
2023-09-26 14:09:54 +0300

    Add files via upload

---
+++ form4/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace form4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtSayi.Location = new System.Drawing.Point(269, 112);
+            this.txtSayi.Multiline = true;
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(102, 32);
+            this.txtSayi.TabIndex = 0;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(420, 118);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(39, 26);
+            this.lblSonuc.TabIndex = 1;
+            this.lblSonuc.Text = "***";
+            this.lblSonuc.Click += new System.EventHandler(this.lblSonuc_Click);
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnHesapla.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHesapla.Location = new System.Drawing.Point(121, 112);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(91, 32);
+            this.btnHesapla.TabIndex = 2;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 214);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ form4/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblSonuc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(txtSayi.Text);
+            int sonuc = sayi * sayi;
+
+            lblSonuc.Text = sonuc.ToString();
+
+
+        }
+    }
+}
